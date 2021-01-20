9149e627fc10c95a0259a65dfbf44987701d32f2
Oxana <oksana.galtsov@gmail.com>
2021-01-20 15:50:58 +0200

    start project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,24 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Gatsby Travel Website`,
+    description: `Travel website showcasing the best travel destinations and deals online.`,
     author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `video`,
+        path: `${__dirname}/src/assets/videos`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -24,11 +32,15 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`,
+      },
+    },
   ],
 }
---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Email from "../components/Email"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Contact = () => (
+  <Layout>
+    <SEO title="Contact Us" />
+    <Email />
+  </Layout>
+)
+
+export default Contact
